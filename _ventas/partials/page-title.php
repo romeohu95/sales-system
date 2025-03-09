d50f6573410a9cdaf267188593ad822ee0d61caa
romeohu95 <romeohu95@gmail.com>
2025-03-09 06:53:51 -0500

    Update page-title.php
    v2

--- _ventas/partials/page-title.php
+++ _ventas/partials/page-title.php
@@ -5,11 +5,11 @@
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
                     <li class="breadcrumb-item"><a href="javascript: void(0);">Hyper</a></li>
-                    <li class="breadcrumb-item"><a href="javascript: void(0);"><?= $subtitle; ?></a></li>
-                    <li class="breadcrumb-item active"><?= $title; ?></li>
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">@@subtitle</a></li>
+                    <li class="breadcrumb-item active"><?php echo $title; ?></li>
                 </ol>
             </div>
-            <h4 class="page-title"><?= $title; ?></h4>
+            <h4 class="page-title"><?php echo $title; ?></h4>
         </div>
     </div>
 </div>
