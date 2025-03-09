0b7316428f700584dc0ea8bdf5c9728ef7184b55
romeohu95 <romeohu95@gmail.com>
2025-03-09 06:54:51 -0500

    Update title-meta.php
    v2

--- _ventas/partials/title-meta.php
+++ _ventas/partials/title-meta.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8" />
-<title><?= $title; ?> | Hyper - Responsive Bootstrap 5 Admin Dashboard</title>
+<title><?php echo $title; ?> | Hyper - Responsive Bootstrap 5 Admin Dashboard</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
 <meta content="Coderthemes" name="author" />
