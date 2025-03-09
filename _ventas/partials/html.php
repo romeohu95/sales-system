33d8d35446a7968763562f2b60cc2ce6a04484e2
romeohu95 <romeohu95@gmail.com>
2025-03-09 06:52:45 -0500

    Update html.php
    v2

--- _ventas/partials/html.php
+++ _ventas/partials/html.php
@@ -1,10 +1,2 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= isset($title) ? $title : 'Default Title'; ?></title>
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <script src="assets/js/scripts.js"></script>
-</head>
-<body>
