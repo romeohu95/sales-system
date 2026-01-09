d80d2f1c5fc9708919ac95a8c8773cce3869e01d
Romeo Bernales Huerta Varillas <romeohu95@gmail.com>
2026-01-09 01:47:06 -0500

    Delete _ventas directory

--- _ventas/includes/auth/check_session.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (!isset($_SESSION['user_id'])) {
-    header("Location: index.php");
-    exit();
-}
-?>
--- _ventas/partials/menu.php
+++
@@ -1,3 +0,0 @@
-<?php include($documentroot.'/partials/topbar.php'); ?>
-
-<?php include($documentroot.'/partials/left-sidebar.php'); ?>
